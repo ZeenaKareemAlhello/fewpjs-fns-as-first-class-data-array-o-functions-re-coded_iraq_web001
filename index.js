0fda039505d7c851dfa6ed5fb799a4050c879448
zena abd alkarem alhello <zena34269@gmail.com>
2020-08-09 19:59:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function wakeDog(dogName, dogBreed){
+      console.log(`Wake ${dogName} the ${dogBreed}`);
+      let message = `Wake ${dogName} the ${dogBreed}`;
+      return message;
+  }
+
+  function leashDog(dogName, dogBreed){
+      console.log(`Leash ${dogName} the ${dogBreed}`);
+      let m = `Leash ${dogName} the ${dogBreed}`;
+      return m;
+  }
+
+  function walkToPark(dogName, dogBreed){
+      console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+      let m = `Walk to the park with ${dogName} the ${dogBreed}`;
+      return m;
+  }
+
+  function throwFrisbee(dogName, dogBreed){
+      console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+      let m = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+      return m;
+  }
+
+  function walkHome(dogName, dogBreed){
+      console.log(`Walk home with ${dogName} the ${dogBreed}`);
+      let m = `Walk home with ${dogName} the ${dogBreed}`;
+      return m;
+  }
+
+  function unleashDog(dogName, dogBreed){
+      console.log(`Unleash ${dogName} the ${dogBreed}`);
+
+}
+
